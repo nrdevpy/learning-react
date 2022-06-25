225360c911ca77f403602ded68ef35940fb12f06
nrdev <nelson_rojas_janda@hotmail.com>
2022-06-25 18:51:21 +0200

    43. Organizing Component Files

---
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -0,0 +1,23 @@
+import ExpenseItem from './ExpenseItem'
+import Card from '../UI/Card'
+import './Expenses.css'
+
+function Expenses(props) {
+    return (
+        <div>
+            <h1>Let's get started!</h1>
+            <Card className="expenses">
+                {props.items.map(item => (
+                    <ExpenseItem
+                        key={item.id}
+                        title={item.title}
+                        amount={item.amount}
+                        date={item.date}
+                    />
+                ))}
+            </Card>
+        </div>
+    )
+}
+
+export default Expenses;
