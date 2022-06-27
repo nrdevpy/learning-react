ed192d136b53fad511bfd12771f8f2a8eea58d96
nrdev <nelson_rojas_janda@hotmail.com>
2022-06-27 12:46:11 +0200

    59. Child-to-parent component communication (bottom-up)

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import ExpenseItem from '../Expenses/ExpenseItem';
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = props => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
@@ -52,7 +52,8 @@ const ExpenseForm = () => {
             amount: enteredAmount,
             date: new Date(enteredDate),
         }
-        console.log(expenseData);
+
+        props.onSaveExpenseData(expenseData);
         setEnteredTitle('');
         setEnteredAmount('');
         setEnteredDate('');
