bb92f48d100c21a3c6dc584a89b18e9ffa9bc836
nrdev <nelson_rojas_janda@hotmail.com>
2022-07-14 14:11:59 +0200

    Working with portals

--- 04-a-complete-practice-project/src/components/UI/ErrorModal.js
+++ 04-a-complete-practice-project/src/components/UI/ErrorModal.js
@@ -1,25 +1,46 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 
 import Card from '../UI/Card'
 import Button from "./Button";
 import classes from './ErrorModal.module.css';
 
+const Backdrop = props => {
+    return (
+        <div className={classes.backdrop} onClick={props.onCloseError} />
+    );
+};
+
+const ModalOverlay = props => {
+    return (
+        <Card className={classes.modal}>
+            <header className={classes.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <div className={classes.content}>
+                <p>{props.message}</p>
+            </div>
+            <footer className={classes.actions}>
+                <Button onClick={props.onCloseError}>Close</Button>
+            </footer>
+        </Card>
+    );
+};
+
 const ErrorModal = props => {
-    console.log(props)
     return (
         <React.Fragment>
-            <div className={classes.backdrop} onClick={props.onCloseError}/>
-            <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={props.onCloseError}>Close</Button>
-                </footer>
-            </Card>
+            {ReactDOM.createPortal(
+                <Backdrop onCloseError={props.onCloseError} />,
+                document.getElementById('backdrop-root')
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay 
+                    title={props.title}
+                    message={props.message}
+                    onCloseError={props.onCloseError}
+                />, document.getElementById('overlay-root')
+            )}
         </React.Fragment>
     );
 };
