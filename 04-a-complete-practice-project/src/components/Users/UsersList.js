6edb49a769c7736f75eaacce508cc723cea40bf1
nrdev <nelson_rojas_janda@hotmail.com>
2022-07-14 18:03:38 +0200

    Users list animation

--- 04-a-complete-practice-project/src/components/Users/UsersList.js
+++ 04-a-complete-practice-project/src/components/Users/UsersList.js
@@ -4,6 +4,10 @@ import Card from '../UI/Card';
 import classes from './UsersList.module.css';
 
 const UsersList = props => {
+
+    if (props.users.length === 0) {
+        return;
+    }
     return (
         <Card className={classes.users}>
             <ul>
